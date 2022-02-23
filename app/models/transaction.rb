90924dbe7d0c9d1dab970c94e70b43c1085b412c
A.Gopa <abdu.q.ah@gmail.com>
2022-02-23 17:13:09 +0200

    add validation over input and output amounts accept values grater than 1

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
   validates_presence_of(:customer_id, :input_amount, :output_amount, :input_currency, :output_currency)
+
+  validates :input_amount, :output_amount, numericality: { greater_than: 1 }
 end
